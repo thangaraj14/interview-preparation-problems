adb694e5e59bd5f9c6b119ef59f94c989467893e
Thangaraj A <thangaraj.a@sap.com>
2019-11-25 19:26:13 +0530

    Modified the code

--- src/geeksforgeeks/AlternateOddAndEvenNumbers.java
+++ src/geeksforgeeks/AlternateOddAndEvenNumbers.java
@@ -5,13 +5,8 @@ package geeksforgeeks;
 
 class AlternateOddAndEvenNumbers {
 
-    // The main function that rearranges elements of given
-    // array. It puts positive elements at even indexes (0,
-    // 2, ..) and negative numbers at odd indexes (1, 3, ..).
     static void rearrange(int arr[], int n) {
-        // The following few lines are similar to partition
-        // process of QuickSort. The idea is to consider 0
-        // as pivot and divide the array around it.
+        //-1, 2, -3, 4, 5, 6, -7, 8, 9
         int i = -1, temp = 0;
         for (int j = 0; j < n; j++) {
             if (arr[j] < 0) {
@@ -22,13 +17,10 @@ class AlternateOddAndEvenNumbers {
             }
         }
 
-        // Now all positive numbers are at end and negative numbers at
-        // the beginning of array. Initialize indexes for starting point
-        // of positive and negative numbers to be swapped
+
         int pos = i + 1, neg = 0;
 
-        // Increment the negative index by 2 and positive index by 1, i.e.,
-        // swap every alternate negative number with next positive number
+
         while (pos < n && neg < pos && arr[neg] < 0) {
             temp = arr[neg];
             arr[neg] = arr[pos];
