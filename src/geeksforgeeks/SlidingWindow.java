0748ce795ffa4d7cb5bced6ada9d422d2dc360fa
vignesh <vigrajar@cisco.com>
2019-12-24 22:24:00 +0530

    new problems

--- src/geeksforgeeks/SlidingWindow.java
+++ src/geeksforgeeks/SlidingWindow.java
@@ -2,35 +2,36 @@ package geeksforgeeks;
 
 import java.util.Deque;
 import java.util.LinkedList;
-
+import java.util.*;
 public class SlidingWindow {
 
     public static void main(String[] args) {
         int arr[] = {8, 5, 10, 7, 9, 4, 15, 12, 90, 13};
         int k = 3;
-        printMax(arr, arr.length, k);
+        maxSlidingWindow(arr, k);
     }
 
-    private static void printMax(int[] arr, int length, int k) {
-        Deque<Integer> deque = new LinkedList<>();
-        int i = 0;
-        for (; i < k; i++) {
-            while (!deque.isEmpty() && arr[i] >= arr[deque.peekLast()])
-                deque.removeLast();
-            deque.addLast(i);
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        if(nums.length==0)return new int[0];
+        List<Integer> result= new ArrayList<>();
+        
+        Deque<Integer> queue= new ArrayDeque<>();
+       // queue.offer(nums[0]);
+        for(int i=0;i<nums.length;i++){
+            while(!queue.isEmpty() && queue.peek()<i-k+1){
+                queue.poll();
+            }
+            while(!queue.isEmpty() && nums[queue.peekLast()]<nums[i]){
+                queue.pollLast();
+            }
+            
+            queue.offer(i);
+            
+            if(i>=k-1){
+                result.add(nums[queue.peekFirst()]);
+            }
         }
-        for (; i < length; i++) {
-            System.out.println(arr[deque.peekFirst()]);
-
-            while (!deque.isEmpty() && deque.peekFirst() <= i - k)
-                deque.removeFirst();
-
-            while (!deque.isEmpty() && arr[i] >= arr[deque.peekLast()])
-                deque.removeLast();
-
-            deque.addLast(i);
-        }
-        System.out.println(arr[deque.peekFirst()]);
+        //int[] res= new int[result.size()];
+        return result.stream().mapToInt(Integer::intValue).toArray();
     }
-
 }
