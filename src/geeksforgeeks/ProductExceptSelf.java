8bddd87c189d322cd77508016d7698d1bdfd2154
vignesh <vigrajar@cisco.com>
2020-01-11 18:22:28 +0530

    new and updated problems

--- src/geeksforgeeks/ProductExceptSelf.java
+++ src/geeksforgeeks/ProductExceptSelf.java
@@ -4,6 +4,20 @@ import java.util.Arrays;
 
 /**
  * https://leetcode.com/problems/product-of-array-except-self/
+ * 
+ * Given numbers [2, 3, 4, 5], regarding the third number 4, 
+ * the product of array except 4 is 2*3*5 which consists of two parts: left 2*3 and right 5.
+ *  The product is left*right. We can get lefts and rights
+ * 
+ * Numbers:     2    3    4     5
+    Lefts:            2  2*3 2*3*4
+    Rights:  3*4*5  4*5    5  
+
+    Let’s fill the empty with 1:
+
+Numbers:     2    3    4     5
+Lefts:       1    2  2*3 2*3*4
+Rights:  3*4*5  4*5    5     1
  */
 public class ProductExceptSelf {
     public static int[] productExceptSelf(int[] nums) {
