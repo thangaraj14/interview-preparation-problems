00bf7bccdcd2f2abc61f8f0977057b01cccca42a
Thangaraj A <thangaraj.a@sap.com>
2020-03-27 12:47:48 +0530

    Adding more problems

--- src/geeksforgeeks/ShuffleArray.java
+++ src/geeksforgeeks/ShuffleArray.java
@@ -1,8 +1,13 @@
 package geeksforgeeks;
 
+import java.util.Arrays;
 import java.util.Random;
 
+/**
+ * https://leetcode.com/problems/shuffle-an-array/
+ */
 public class ShuffleArray {
+
     private int[] nums;
     private Random random;
 
@@ -42,6 +47,7 @@ public class ShuffleArray {
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3 };
         ShuffleArray sa = new ShuffleArray(arr);
-        sa.shuffle();
+        System.out.println("After shuffling :" + Arrays.toString(sa.shuffle()));
+        System.out.println("Reset : " + Arrays.toString(sa.reset()));
     }
 }
