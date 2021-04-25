2ab36ebb097453dc37a9af51da0bd79ff7e73d1d
Vignesh <vicky209180@gmail.com>
2021-04-25 11:44:48 +0530

    refactored packages

--- out/production/Problems/SQL/DeleteDuplicate.sql
+++
@@ -1,5 +0,0 @@
-
-
--- the below command will do cross product and generate all combinations of rows
--- we are selecting a row with same Email and Greater Id
-DELETE p1 from Person p1, Person p2 where p1.Email=p2.Email and p1.Id>p2.Id
--- out/production/Problems/SQL/EmployeeEarningHigherThanManagers.sql
+++
@@ -1,3 +0,0 @@
-
-
-select tb1.Name as Employee from Employee tb1 inner join Employee tb2 on tb1.ManagerId=tb2.Id where tb1.Salary>tb2.Salary
